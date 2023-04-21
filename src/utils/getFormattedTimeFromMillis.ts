7994c13393b0d3622141c30800dea855d3d81659
dikum98 <dikum98@gmail.com>
2023-04-21 10:27:45 +0900

    fix: 시간 포맷팅 유틸함수 오류 수정
    - month가 한 달씩 당겨지는 오류 수정

--- src/utils/getFormattedTimeFromMillis.ts
+++ src/utils/getFormattedTimeFromMillis.ts
@@ -3,7 +3,7 @@ import addLeadingZero from './addLeadingZero';
 export default function getFormattedTimeFromMillis(millis: number): string {
   const date = new Date(millis);
 
-  return `${date.getFullYear()}.${addLeadingZero(date.getUTCMonth())}.${addLeadingZero(
+  return `${date.getFullYear()}.${addLeadingZero(date.getUTCMonth() + 1)}.${addLeadingZero(
     date.getUTCDate()
   )}.${addLeadingZero(date.getHours())}:${addLeadingZero(date.getUTCMinutes())}`;
 }
