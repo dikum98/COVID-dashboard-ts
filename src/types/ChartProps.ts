6d630f1e2f79df88f0eb42a91c40c729e5b7fdc7
dikum98 <dikum98@gmail.com>
2023-04-21 14:23:38 +0900

    remove: 이름 변경으로 삭제된 파일 커밋

--- src/types/ChartProps.ts
+++
@@ -1,5 +0,0 @@
-import TotalCounts from './TotalCounts';
-
-export default interface ChartProps {
-  country: string;
-}
--- src/types/TotalCounts.ts
+++
@@ -1,9 +0,0 @@
-export default interface TotalCounts {
-  cases: number;
-  todayCases: number;
-  deaths: number;
-  todayDeaths: number;
-  recovered: number;
-  todayRecovered: number;
-  updatedTime: number;
-}
